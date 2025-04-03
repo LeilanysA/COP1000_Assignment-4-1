c2ca3661c4bc92962b35daaf06ad0eba5eb04c48
LeilanysA <larzate@hawkmail.hccfl.edu>
2025-04-03 15:07:21 -0400

    4-1

--- main.py
+++ main.py
@@ -4,10 +4,13 @@ This program calculates prices for custom house signs.
 
 # Declare and initialize variables here.
 # Charge for this sign.
+charge = 0.00
 # Number of characters.
+numChars = 8
 # Color of characters.
+color = "gold"
 # Type of wood.
-
+woodType = "oak"
 # Write assignment and if statements here as appropriate.
 
 # Output Charge for this sign.
